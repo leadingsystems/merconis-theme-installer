e40f5069c204024b3512e0c46da2b3cad4d8945b
Daniel Bitsch <bitsch@leadingsystems.de>
2024-07-22 17:01:56 +0200

    fix some Contao 5 issues

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -5,9 +5,9 @@ namespace Merconis\ThemeInstaller;
 use Contao\System;
 use Symfony\Component\HttpFoundation\Request;
 
-//if (System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest(System::getContainer()->get('request_stack')->getCurrentRequest() ?? Request::create(''))) {
-//	$GLOBALS['TL_CSS'][] = 'bundles/leadingsystemsmerconiscustom/be/css/style.css';
-//}
+if (System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest(System::getContainer()->get('request_stack')->getCurrentRequest() ?? Request::create(''))) {
+	$GLOBALS['TL_CSS'][] = 'bundles/leadingsystemsmerconiscustom/be/css/style.css';
+}
 
 // API
 $GLOBALS['LS_API_HOOKS']['apiReceiver_processRequest'][] = array('LeadingSystems\MerconisThemeInstallerBundle\API\APIGeneral', 'processRequest');
